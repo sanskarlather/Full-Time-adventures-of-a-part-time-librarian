0e53e142ed98d65c92741d77932f1c6a057c2fc3
sanskar <lathersanskar@gmail.com>
2020-11-09 21:43:17 +0530

    update main

--- main.c
+++ main.c
@@ -12,8 +12,14 @@ static bool getInput(void)
 
 int main()
 {
-   printf("Welcome to Little Cave Adventure.\n");
-   while (parseAndExecute(input) && getInput());
-   printf("\nBye!\n");
-   return 0;
+    printf("Welcome to the Full-Time Adventures of a Part-Time Librarian\n");
+
+    printf("You wake up and see your alarm telling you to get ready and head to your boring, mundane, and part-time job as a librarian \n");
+    printf("Everyday is the same, everyday you just sit on your thumbs at the library and wait for people to come and get their books. \n Everyday you just sit there hoping for something exciting to happen, but nothing ever does\n");
+
+    while (parseAndExecute(input) && getInput());
+    printf("\nBye!\n");
+    return 0;
+
+  
 }
